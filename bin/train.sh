84ad4239b3fcfca082d8e4c8b70f9d1961b52af2
Israel Herraiz <ihr@google.com>
2020-01-24 21:49:38 +0100

    WIP

--- bin/train.sh
+++ bin/train.sh
@@ -1,1 +1,12 @@
-gcloud ai-platform jobs submit
+gcloud ai-platform jobs submit generator_train_`date +"%s"` \
+  --python-version=3.7 \
+  --runtime-version=1.15 \
+  --scale-tier basic \
+  --package-path ./trainer \
+  --module-name trainer.task \
+  --job-dir=gs://ihr-kschool-training/tmp/ \
+  --region us-central1 \
+  
+
+  
+  
