444545f5e62269272c403770c9152ca7b80329a9
Israel Herraiz <ihr@google.com>
2020-01-25 12:42:43 +0100

    Even less resources

--- bin/train_hyper.sh
+++ bin/train_hyper.sh
@@ -1,7 +1,7 @@
 gcloud ai-platform jobs submit training generator_train_ht_`date +"%s"` \
   --python-version=3.7 \
   --runtime-version=1.15 \
-  --scale-tier STANDARD_1 \
+  --scale-tier BASIC \
   --package-path ./trainer \
   --module-name trainer.task \
   --job-dir=gs://ihr-kschool-training/tmp/ \
