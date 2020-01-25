c5bab4e7bb6875c58f5dc915f11a2cc3b7b1bad7
Israel Herraiz <ihr@google.com>
2020-01-25 12:40:26 +0100

    Reduce resources demand

--- bin/train_hyper.sh
+++ bin/train_hyper.sh
@@ -1,7 +1,7 @@
 gcloud ai-platform jobs submit training generator_train_ht_`date +"%s"` \
   --python-version=3.7 \
   --runtime-version=1.15 \
-  --scale-tier basic \
+  --scale-tier STANDARD_1 \
   --package-path ./trainer \
   --module-name trainer.task \
   --job-dir=gs://ihr-kschool-training/tmp/ \
